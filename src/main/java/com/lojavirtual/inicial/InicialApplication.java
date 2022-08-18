8759567f63e8e1af731b9534d4f59b9d95b28d39
souza99 <jpfsouza99@gmail.com>
2022-08-18 20:12:49 -0300

    Iniciando projeto

---
+++ src/main/java/com/lojavirtual/inicial/InicialApplication.java
@@ -0,0 +1,13 @@
+package com.lojavirtual.inicial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InicialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InicialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lojavirtual/inicial/InicialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lojavirtual.inicial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InicialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
